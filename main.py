2c4714fcebe3f1e4add76cad6b5ac1817f658ac9
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-08-29 19:12:24 -0700

    adds

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tinygrad.tensor import Tensor
 
-tensor = Tensor([1, 2, 3, 4, 5, 6])
-tensor = tensor.cos()
+tensor = Tensor([-1, -5, 1, 2, 3, 4, 5])
+tensor = tensor.exp()
 
 print(tensor.numpy())
